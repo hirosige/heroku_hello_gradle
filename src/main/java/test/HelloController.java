cf036bd521f28fe2145a3de5fbb2a9a3ffbadc59
hiroshige <hiroshige.negishi@arms-asia.com>
2016-08-08 18:24:12 +0700

    deploy test

--- src/main/java/test/HelloController.java
+++ src/main/java/test/HelloController.java
@@ -1,9 +1,10 @@
 package test;
 
-        import org.springframework.stereotype.Controller;
-        import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-        import org.springframework.web.bind.annotation.RequestMapping;
-        import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.stereotype.Controller;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.Date;
 
 @Controller
 @EnableAutoConfiguration
@@ -12,6 +13,7 @@ public class HelloController
     @RequestMapping("/")
     @ResponseBody
     public String home() {
-        return "Hello, Spring Boot Sample Application!! Deployed!!";
+        Date date = new Date();
+        return "[" + date.toString() +  "] Hello, Spring Boot Sample Application!! Deployed!!";
     }
 }
